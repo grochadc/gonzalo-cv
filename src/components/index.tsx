cdfea4e0ba44a2b651e0150e09110ea1d5a4c4ec
Gonzalo <grochadc@gmail.com>
2022-04-17 08:41:34 -0500

    Include more icons

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,4 @@ import tw from "tailwind-styled-components";
 
 export const Link = tw.a`underline text-blue`;
 
-export const Section = tw.section`flex flex-col items-center w-2/3 py-10`;
+export const Section = tw.section`flex flex-col items-center w-2/3 mx-auto my-12`;
--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -1,7 +1,31 @@
 import ProfilePic from "../profile.jpeg";
 import TwitterTile from "../icons/twitter-tile.svg";
 import GithubTile from "../icons/github-tile.svg";
+import GmailTile from "../icons/gmail.svg";
+import WhatsappIcon from "../icons/whatsapp.svg";
+import MexicoFlag from "../icons/Flag_of_Mexico.svg";
+import USFlag from "../icons/Flag_of_the_United_States.svg"
 import {Link} from "../components";
+
+const ContactLlinks: ListItem[] = [
+    {name: "github", icon: GithubTile, href:"https://github.com/grochadc", children: ['grochadc']},
+    {name:"twitter", icon: TwitterTile, href:"https://twitter.com/medicengonzo", children: ['@medicengonzo']},
+    {name: "gmail", icon: GmailTile, href:"mailto:grochadc@gmail.com", children: ['grochadc@gmail.com']},
+    {name: "telefono", icon: WhatsappIcon, href:"+523121710536", children: ['+52 312 171 0536']}
+]
+
+type ListItem = {name: string; icon: string; href: string; children?: any;}
+const ContactInfo = (props: {list: ListItem[]}) => {
+    return (<ul className="my-2 flex justify-around">
+        {props.list.map((listItem, index) => <li className="flex">
+            <img className="h-6 mr-2" src={listItem.icon} alt={`${listItem.name} icon`} /><Link href={listItem.href}>{listItem.children}</Link>
+        </li>)}
+    </ul>)
+}
+
+const Flag = (props: {src: any}) => {
+    return (<><img src={props.src} alt="" className="h-3 ml-1" /></>)
+}
 
 const Header = () => (
     <header className='bg-fogra h-1/3vh flex justify-between p-12'>
@@ -18,14 +42,11 @@ const About = () => (
             <h2 className="text-4xl font-bold mb-2">Acerca de Mí</h2>
             <p>Mi nombre es Gonzalo Rocha. Estudié la licenciatura en Ciencias de la Comunicación pero me ha apasionado las tecnologías web desde mi adolescencia.
             Desde 2011 he aprendido de forma autodidacta diferentes tecnologías para el desarrollo y diseño web y he mantenido diversos proyectos personales.</p>
-            <div className="flex w-80 justify-around mt-5">
-                <span className="flex"><img className="h-6 mr-2" src={TwitterTile} alt="twitter icon"/><Link href="https://twitter.com/medicengonzo">@medicengonzo</Link></span>
-                <span className="flex"><img className="h-6 mr-2" src={GithubTile} alt="github icon" /><Link href="https://github.com/grochadc">grochadc</Link></span>
-            </div>
+            <ContactInfo list={ContactLlinks} />
         </div>
         <div className="text-center relative top-36 w-48 right-2">
-            <p>Mexicano 🇲🇽</p>
-            <p>Español 🇲🇽, Inglés 🇺🇸</p>
+            <div className="flex items-center">Mexicano<Flag src={MexicoFlag} /></div>
+            <div className="flex items-center">Español<Flag src={MexicoFlag} />, Inglés<Flag src={USFlag} /></div>
             <p>🎥 📕 💻</p>
         </div>
   </section>
